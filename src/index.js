116de383f980eb589010eb6358d283fd10f09eb5
Prathyushkotla96 <33094244+Prathyushkotla96@users.noreply.github.com>
2020-01-31 17:22:01 -0600

    created a server where user can send message to all the users connected to server

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const http=require('http')
+const express= require('express')
+const path=require('path')
+const socketio=require('socket.io')
+
+
+const app=express()
+const server=http.createServer(app)
+const io=socketio(server)
+
+const port=process.env.PORT || 3000
+const publicDirectoryPath=path.join(__dirname,'../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection',(socket)=>{
+    console.log('new web socket collection')
+
+    socket.emit('message','welcome')
+    socket.broadcast.emit('message','A new user has joined')
+
+    socket.on('sendMessage',(message)=>{
+        io.emit('message',message)
+
+    })
+
+    socket.on('sendLocation',(coords)=>{
+        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+
+    })
+
+    socket.on('disconnect',()=>{
+        io.emit('message','user has left')
+    })
+})
+server.listen(port,()=>{
+    console.log(`server is up on port ${port}!`)
+
+})
